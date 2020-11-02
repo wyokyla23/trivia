18ad57f3b0402a4f84b95132d2f7b92906585b3a
wyokyla23 <wyokyla23@gmail.com>
2020-11-01 20:53:56 -0600

    added pages to the app

---
+++ src/pages/PlayerScore/index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { Card } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#8f935c',
+    height: '100vh',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh'
+  },
+  text: {
+    color: 'black',
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginBottom: '25px',
+    fontSize: '1.5rem'
+  },
+  button: {
+    minWidth: '200px',
+    margin: '10px',
+    border: 'solid #504d31 ',
+  },
+})
+
+
+function PlayerScore({ history, match }) {
+  const score = history.location.state?.score || 0;
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <Container
+        className={classes.container}
+      >
+        <Grid container className={classes.header}>
+          <Typography
+            className={classes.text}
+            variant='h3'
+          >
+            Thanks for taking the quiz
+            </Typography>
+          <Typography
+            className={classes.text}
+            variant='h3'
+          >
+            You scored {score} out of 21
+            </Typography>
+
+        </Grid>
+        <Button
+          size="large"
+          className={classes.button}
+          onClick={() => history.push('/')}>
+          <Typography className={classes.answers}>
+            Play Again
+              </Typography>
+        </Button>
+      </Container>
+    </div>
+  )
+}
+
+export default PlayerScore
---
+++ src/pages/Trivia/index.js
@@ -0,0 +1,123 @@
+import React, { useState } from 'react'
+import propTypes from 'prop-types'
+import data from '../../data.json'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import Container from '@material-ui/core/Container'
+import shuffle from 'lodash/shuffle'
+import QuestionCard from '../../components/QuestionCard'
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#8f935c',
+    height: '100vh',
+  },
+  quizContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    height: '100vh'
+  },
+  header: {
+    justifyContent: 'space-around',
+    marginBottom: '25px',
+    fontSize: '1.5rem'
+  },
+  response: {
+    marginTop: '35px',
+    fontSize: '2rem'
+  }
+})
+
+const PropTypes = {
+  history: propTypes.object
+}
+
+export default function Trivia({ history, match }) {
+  const [score, setScore] = useState(0)
+  const [questionIndex, setQuestionIndex] = useState(0)
+
+  const [currentQuestion, setCurrentQuestion] = useState(
+    data[questionIndex].question
+  )
+  const [currentAnswers, setCurrentAnswers] = useState(
+    data[questionIndex].incorrect.concat(data[questionIndex].correct)
+  )
+
+  const [questionMessage, setQuestionMessage] = useState(null)
+  const classes = useStyles()
+
+  const correctAnswer = data[questionIndex].correct
+
+  const isAnswerCorrect = (answer) => {
+    return correctAnswer === answer
+  }
+
+  const handleAnswer = (answer) => {
+
+    const correct = isAnswerCorrect(answer)
+    if (correct) {
+      setScore(prev => prev + 1)
+    }
+    setQuestionMessage(correct)
+    setTimeout(
+      () => {
+        if (questionIndex === data.length - 1) {
+          history.push(
+            '/player-score',
+            {
+              score: correct
+                ? score + 1
+                : score
+            }
+          )
+        } else {
+          setQuestionMessage(null)
+          setQuestionIndex(
+            (prev) => prev < data.length
+              ? prev + 1
+              : 0
+          )
+          const { question, incorrect, correct } = data[questionIndex + 1];
+          setCurrentQuestion(question)
+          setCurrentAnswers(incorrect.concat(correct))
+        }
+      },
+      3000
+    )
+  }
+
+  return (
+    <div className={classes.root}>
+      <Container
+        className={classes.quizContainer}>
+        <Grid
+          container
+          className={classes.header}>
+          <Grid item>
+            Question: {questionIndex + 1} / {data.length}
+          </Grid>
+          <Grid item>Score: {score}</Grid>
+        </Grid>
+        <QuestionCard
+          question={currentQuestion}
+          answers={shuffle(currentAnswers)}
+          handleAnswer={handleAnswer}
+          questionMessage={questionMessage}
+        />
+        <Typography className={classes.response}>
+          {questionMessage !== null ? (
+            questionMessage === true
+              ? "Correct! You're Amazing!"
+              : `Ooh, sorry. The correct answer is: ${correctAnswer}`
+          ) : ' '}
+        </Typography>
+      </Container>
+    </div >
+  )
+}
